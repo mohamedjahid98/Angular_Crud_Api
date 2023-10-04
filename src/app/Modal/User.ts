a1f44fcb1d70b551a31c0d6f062da3e4c95bf71f
Mohamed Jahid <70051721+mohamedjahidofficial@users.noreply.github.com>
2023-10-04 08:18:45 +0530

    initial commit

---
+++ src/app/Modal/User.ts
@@ -0,0 +1,10 @@
+export interface UserData {
+    users: UserData[];
+    id: number;
+    firstName: any;
+    lastName: string;
+    age: number;
+    email: string;
+    gender: string;
+    phone: number;
+  }
